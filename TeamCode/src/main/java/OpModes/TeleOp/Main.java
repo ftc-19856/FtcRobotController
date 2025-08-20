0e7df25664c0cea2901ab04d8066ae95e1f84324
RibsTheGreat <fleskessm@gmail.com>
2025-08-20 15:30:05 -0700

    Changed to OpMode

--- TeamCode/src/main/java/OpModes/TeleOp/Main.java
+++ TeamCode/src/main/java/OpModes/TeleOp/Main.java
@@ -2,6 +2,7 @@ package OpModes.TeleOp;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
@@ -9,11 +10,12 @@ import com.qualcomm.robotcore.hardware.IMU;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
+
 import Components.Drive;
 import Util.Vector2;
 
 @TeleOp(name="Main", group="TeleOp")
-public class Main extends LinearOpMode {
+public class Main extends OpMode {
     Drive drive;
     DcMotorEx frontLeftMotor;
     DcMotorEx backLeftMotor;
@@ -22,7 +24,7 @@ public class Main extends LinearOpMode {
     IMU imu;
 
     @Override
-    public void runOpMode() {
+    public void init() {
         imu = hardwareMap.get(IMU.class, "imu");
         frontLeftMotor = hardwareMap.get(DcMotorEx.class, "frontLeftMotor");
         backLeftMotor = hardwareMap.get(DcMotorEx.class, "backLeftMotor");
@@ -31,10 +33,23 @@ public class Main extends LinearOpMode {
 
         drive = new Drive(frontLeftMotor, frontRightMotor, backLeftMotor, backRightMotor, imu);
 
-        waitForStart();
+        backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        while (opModeIsActive()){
+
+        telemetry.addData("Status:", "Initialized");
+        telemetry.update();
+    }
+
+    @Override
+    public void loop() {
+            telemetry.addData("Status", "Running");
+            telemetry.update();
+
             if (!gamepad2.y) {
+                telemetry.addData("Main drive enabled", (!gamepad2.y));
                 Vector2 driveDirection = new Vector2(-gamepad1.left_stick_y, gamepad1.left_stick_x);
                 float driveRotation = gamepad1.right_stick_x;
                 drive.moveInDirection(driveDirection, driveRotation, 1.0f);
@@ -43,6 +58,6 @@ public class Main extends LinearOpMode {
             Vector2 preciseDirection = new Vector2(-gamepad2.left_stick_y, gamepad2.left_stick_x);
             float preciseRotation = gamepad2.right_stick_x * 0.6f;
             drive.moveInDirection(preciseDirection, preciseRotation, 0.35f);
-        }
+
     }
 }
