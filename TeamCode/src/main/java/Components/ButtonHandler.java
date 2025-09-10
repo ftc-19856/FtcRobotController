987e138b62d58315186f045b132730ca019ebb9c
RibsTheGreat <fleskessm@gmail.com>
2025-09-09 17:00:30 -0700

    Changed button handler

--- TeamCode/src/main/java/Components/ButtonHandler.java
+++ TeamCode/src/main/java/Components/ButtonHandler.java
@@ -1,16 +1,30 @@
-package Util;
+package Components;
 
 // Gamepad toggle class
 public class ButtonHandler {
 
-    private boolean currentState;
+    private boolean currentValue;
     private boolean previousState;
 
+    // Initializes the variables
     public ButtonHandler(boolean initialValue) {
-        this.currentState = initialValue;
+        this.currentValue = initialValue;
         this.previousState = false;
     }
 
-    public boolean update()
+    // Assigns a button to be toggled and adds the logic
+    public boolean update(boolean currentButtonState) {
+        if(currentButtonState && !previousState){
+            currentValue = !currentValue;
+        }
+
+        previousState = currentButtonState;
+        return currentValue;
+    }
+
+    // Gets the current state of the button for telemetry
+    public boolean getValue(){
+        return currentValue;
+    }
 
 }
